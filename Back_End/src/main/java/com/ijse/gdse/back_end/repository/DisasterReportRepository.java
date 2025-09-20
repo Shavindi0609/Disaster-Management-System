3446fef4c8d9e6ebd746711c52748f4d739f0f51
#Shanu0609@ <shavindialoka69@gmail.com>
2025-09-21 04:15:59 +0530

    Delete Disaster Report

--- Back_End/src/main/java/com/ijse/gdse/back_end/repository/DisasterReportRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ijse.gdse.back_end.repository;
-
-import com.ijse.gdse.back_end.entity.DisasterReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DisasterReportRepository extends JpaRepository<DisasterReport, Long> {
-}
